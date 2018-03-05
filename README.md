32b19e382467b1bc5221066ba98866053e6016be
Samuel Butta <samuel@butta.cz>
2018-03-05 12:50:20 +0100

    Add basic structures

--- README.md
+++ README.md
@@ -15,3 +15,25 @@ val age = 12
 var nameMutable: String = "Jan"
 nameMutable = "Jan Novak"
 ```
+
+Basic structures
+--
+
+Basic control structures are if, for, match, try and block.
+
+```scala
+    val x: Int = 7;
+    val y: Int = 10;
+    val max: Int = if (x > y) x else y
+
+    val numbers : Array[Int] = Array(1, 9, 6, 9, 2)
+    for(i <- numbers) {
+      println(i)
+    }
+```
+
+For can produce collection by the keyword yield.
+```scala
+    val numbersSquare = for(i <- numbers) yield i * i
+    numbersSquare.foreach(n => println(n))
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,17 +4,32 @@
 object Main {
 
   def main(args: Array[String]): Unit = {
-
     variables()
-
+    basicStructures()
   }
 
-  def variables() : Unit = {
+  def variables(): Unit = {
     val name: String = "Jan"
     val age = 12
 
     var nameMutable: String = "Jan"
     nameMutable = "Jan Novak"
+  }
+
+
+  def basicStructures(): Unit = {
+    val x: Int = 7;
+    val y: Int = 10;
+    val max: Int = if (x > y) x else y
+
+    val numbers : Array[Int] = Array(1, 9, 6, 9, 2)
+    for(i <- numbers) {
+      println(i)
+    }
+
+    //for can produce collection by the keyword yield
+    val numbersSquare = for(i <- numbers) yield i * i
+    numbersSquare.foreach(n => println(n))
   }
 
 }
